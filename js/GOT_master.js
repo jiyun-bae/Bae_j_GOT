58dcadb1fed3773b580caac185b7221ff51e325c
Jiyun <baejiyun01@gmail.com>
2020-08-09 02:49:22 +0900

    Updating js again

--- js/GOT_master.js
+++ js/GOT_master.js
@@ -9,6 +9,8 @@
 	    houseName = document.querySelector(".house-name"),
 	    houseBio = document.querySelector(".house-info");
 
+	var x = document.getElementById("myVideo");
+
 	const houseInfo = [
 	["Stark", 'House Stark of Winterfell is a Great House of Westeros, ruling over the vast region known as the North from their seat in Winterfell. It is one of the oldest lines of Westerosi nobility by far, claiming a line of descent stretching back over eight thousand years. Before the Targaryen conquest, as well as during the War of the Five Kings and Daenerys Targaryens invasion of Westeros, the leaders of House Stark ruled over the region as the Kings in the North.'],
 	["Baratheon", 'House Baratheon of Storms End is a legally extinct Great House of Westeros. A cadet branch was formerly the royal house, but House Lannister now controls the throne. House Baratheon traditionally ruled the Stormlands on the eastern coast of Westeros, aptly named for its frequent storms, from their seat of Storms End. House Baratheon became the royal house of the Seven Kingdoms after Robert Baratheon led a rebellion against the Targaryen dynasty. At the end of the rebellion, Robert ascended the Iron Throne as Robert I and married Cersei Lannister after the death of Lyanna Stark.'],
@@ -18,13 +20,15 @@
 	["Arryn", 'House Arryn of the Eyrie is one of the Great Houses of Westeros. It has ruled over the Vale of Arryn for millennia, originally as the Kings of Mountain and Vale and more recently as Lords Paramount of the Vale and Wardens of the East under the Targaryen kings and Baratheon-Lannister kings. The nominal head of House Arryn is Robin Arryn, the Lord of the Eyrie, with his stepfather Petyr Baelish acting as Lord Protector until he reaches the age of majority.']
 ]
 
+    function myVideo() {
+	    x.volume = 1.0;
+}
+
 	function showLightbox() {
 		lightBox.classList.add('show-lightbox');
 
 		let house = this.classList[1],
 		    targetSource = house.charAt(0).toUpperCase() + house.slice(1);
-
-		
 		
 		// debugger;
 		lbVideo.play();
